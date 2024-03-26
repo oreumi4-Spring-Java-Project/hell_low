31d21bda4c467d7361f23fc8c3dcea4190445c7d
Jaewon <choijawon99@gmail.com>
2024-03-26 17:44:13 +0900

    [ADD] 기본 세팅
    - 의존성 추가
    - 기본 디렉토리 구성

---
+++ HellLow/src/main/java/com/est/helllow/HellLowApplication.java
@@ -0,0 +1,13 @@
+package com.est.helllow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellLowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellLowApplication.class, args);
+    }
+
+}
---
+++ HellLow/src/test/java/com/est/helllow/HellLowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.est.helllow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellLowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
