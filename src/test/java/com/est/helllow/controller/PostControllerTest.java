f964b911beff9192d7b3d3e7d4a6f986eb0d827b
Jaewon <choijawon99@gmail.com>
2024-03-28 15:12:10 +0900

    [TEST] post 등록 기능 test 구현

---
+++ src/test/java/com/est/helllow/controller/PostControllerTest.java
@@ -0,0 +1,74 @@
+package com.est.helllow.controller;
+
+import com.est.helllow.domain.Post;
+import com.est.helllow.domain.dto.AddPostRequest;
+import com.est.helllow.repository.PostRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class PostControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @BeforeEach
+    public void mockMvcSetUp(){
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        postRepository.deleteAll();
+    }
+
+    @DisplayName("블로그 글 추가 성공")
+    @Test
+    public void addPost() throws Exception{
+        //given
+        String url = "/api/post";
+
+        String category = "category";
+        String title = "title";
+        String content = "contents";
+        AddPostRequest request = new AddPostRequest(category,title, content);
+
+        String requestBody = objectMapper.writeValueAsString(request);
+
+        //when
+        ResultActions result = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(requestBody));
+
+        //then
+        result.andExpect(status().isCreated());
+        List<Post> postList = postRepository.findAll();
+
+        assertThat(postList.size()).isEqualTo(1);
+        assertThat(postList.get(0).getPostTitle()).isEqualTo(title);
+        assertThat(postList.get(0).getPostContent()).isEqualTo(content);
+    }
+}
