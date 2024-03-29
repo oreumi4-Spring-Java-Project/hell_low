b5a811f548ddaf853f3087358a86ad0c416b4a7d
Kimmingyu <aabbb0830@naver.com>
2024-03-29 14:54:32 +0900

    [FEAT] 내가 작성한 댓글 보기 기능 구현.

--- src/main/java/com/est/helllow/repository/ReplyRepository.java
+++ src/main/java/com/est/helllow/repository/ReplyRepository.java
@@ -1,5 +1,6 @@
 package com.est.helllow.repository;
 
+import com.est.helllow.domain.Post;
 import com.est.helllow.domain.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +9,5 @@ import java.util.List;
 
 @Repository
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
-
+    List<Reply> findByUser_userId(Long userId);
 }
