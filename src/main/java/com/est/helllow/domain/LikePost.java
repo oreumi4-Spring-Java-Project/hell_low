501c00d31b0d6c201ada55ace924037de337956c
임성현 <shlim0287@gmail.com>
2024-03-31 01:08:29 +0900

    [FEAT] AUDITING 설정 추가.

--- src/main/java/com/est/helllow/domain/LikePost.java
+++ src/main/java/com/est/helllow/domain/LikePost.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor
 @Table(name = "LIKE_POST") // MYSQL 에서 LIKE가 예약어
+@EntityListeners(AuditingEntityListener.class)
 public class LikePost {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
