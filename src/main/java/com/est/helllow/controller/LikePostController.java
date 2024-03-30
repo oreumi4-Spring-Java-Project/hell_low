2dad83db51ef31bf93f87e3065c589df81bc19e6
임성현 <shlim0287@gmail.com>
2024-03-31 01:19:21 +0900

    [FEAT] 게시물 좋아요 추가 기능 구현.

---
+++ src/main/java/com/est/helllow/controller/LikePostController.java
@@ -0,0 +1,34 @@
+package com.est.helllow.controller;
+
+import com.est.helllow.domain.LikePost;
+import com.est.helllow.service.LikePostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("api/post/{postId}/like/{userId}")
+public class LikePostController {
+
+    private final LikePostService likePostService;
+
+    /**
+     * 게시물 좋아요 API
+     * userId 로그인 기눙 구현 시 구조 수정 예정
+     *
+     * @param postId
+     * @param userId
+     * @return
+     */
+    @PostMapping
+    public ResponseEntity<LikePost> likePost(@PathVariable(name = "postId") Long postId,
+                                                        @PathVariable(name = "userId") Long userId) {
+        LikePost likePost = likePostService.likePost(postId, userId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(likePost);
+    }
+}
